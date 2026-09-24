128f098b1b3b58c6443809c85ba7b89eed82aa6c
Adyzzz <aleong2400@alumno.ipn.mx>
2026-09-24 16:30:23 -0600

    Ejercicios 02-objetos

--- PrimerParcial/02_Ejercicios/js/app.js
+++ PrimerParcial/02_Ejercicios/js/app.js
@@ -64,4 +64,56 @@ formArreglos.addEventListener('submit', (evento) => {
     resultadoArreglos.textContent = resultado;
 });
 
+// Ejercicio de objetos
+const formularioObjetos = document.getElementById('form-objeto');
+const resultadoObjeto = document.getElementById('resultado-objeto');
+
+formularioObjetos.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    // necesitamos contruir el objetp del taller
+    const taller ={
+        nombre : document.getElementById('obj-nombre').value,
+        instructor : document.getElementById('obj-instructor').value,
+        cupo : Number(document.getElementById('obj-cupo').value),
+        inscritos : Number(document.getElementById('obj-inscritos').value)
+    };
+
+    const operacion =  document.getElementById('operacion-objeto').value;
+
+    let resultado;
+
+    switch(operacion){
+        case 'keys':
+            resultado = JSON.stringify(Object.keys(taller));
+            break;
+        case 'values':
+            resultado = JSON.stringify(Object.values(taller));
+            break;
+
+        case 'entries':
+            resultado = JSON.stringify(Object.entries(taller));
+            break;
+
+        case 'stringify':
+            const textoJson = JSON.stringify(taller, null, 2);
+            resultado = `${textoJson}\n \n tipo: ${typeof textoJson}`;
+            break;
+            
+        case 'roundtrip':
+            const textoJsons = JSON.stringify(taller, null, 2);
+            const objetoDevuelta = JSON.parse(textoJsons);
+
+            resultado = [
+                textoJsons, 
+                '',
+                `tipo:${typeof objetoDevuelta}`,
+                objetoDevuelta.nombre
+            ].join('\n');
+            break;
+    }
+    resultadoObjeto.textContent = resultado;
+})
+
+
 pintarTabla();
